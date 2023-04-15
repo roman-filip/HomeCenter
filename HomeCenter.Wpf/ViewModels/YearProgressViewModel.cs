92f1f00bae4944fd6aea5d9a939c56418088dcb8
Roman Filip <dicky@email.cz>
2023-04-15 11:45:11 +0200

    Implementation of YearProgress

---
+++ HomeCenter.Wpf/ViewModels/YearProgressViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace HomeCenter.Wpf.ViewModels
+{
+    public partial class YearProgressViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _yearProgress = "0";
+
+        public YearProgressViewModel()
+        {
+            SetYearProgress();
+        }
+
+        private void SetYearProgress()
+            => YearProgress = double.Round((double)DateTime.Now.DayOfYear / CultureInfo.InvariantCulture.Calendar.GetDaysInYear(DateTime.Now.Year) * 100, 0).ToString();
+    }
+}
